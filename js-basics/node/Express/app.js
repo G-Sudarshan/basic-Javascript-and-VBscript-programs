ec5f010dafb0e56cf3c46b7477558d7ad0e436d4
deepin pc <11sudarshang@gmail.com>
2020-07-20 06:26:59 +0530

    gym webpage created

--- js-basics/node/Express/app.js
+++ js-basics/node/Express/app.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 const app = express();
 port = 80;
-app.use("/static", express.static("static")); //static files
 
+//EXPRESS SPECIFIC STUFF
+
+app.use("/static", express.static("static")); //static files
+app.use(express.urlencoded());
+//PUG SPECIFIC STUFF
 //set the template engine as pug
 app.set("view engine", "pug");
 
@@ -11,28 +16,25 @@ app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 
 //our pug demo endpoint
-app.get("/demo", (req, res) => {
-  res.render("demo", {
-    title: "Hey harry",
-    message: "Hello there! Thanks for tell how to use pug",
-  });
-});
-
+//ENDPOINTS
 app.get("/", (req, res) => {
-  res.status(200).send("This is homepage of first app");
+  const con = "This is the best content";
+  const params = { title: "pubg is the best game", content: con };
+  res.status(200).render("index.pug", params);
 });
 
-app.get("/about", (req, res) => {
-  res.send("This is about page");
+app.post("/", (req, res) => {
+  name = req.body.name;
+  age = req.body.age;
+  gender = req.body.gender;
+  let outputToWrite = `The name of client is ${name}, the age of cleint is ${age}, the gender of client is ${gender}`;
+  fs.writeFileSync("output.txt", outputToWrite);
+
+  const params = { message: "your form has been submitted succefully" };
+  res.status(200).render("index.pug", params);
 });
 
-app.post("/about", (req, res) => {
-  res.send("This is about page from post method");
-});
-
-app.get("/this", (req, res) => {
-  res.status(404).send("This page is not found");
-});
+//START THE SERVER
 
 app.listen(port, () => {
   console.log(`The application statrted successfully on port ${port}`);
